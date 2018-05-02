969cde26e4f1e2e790a9069a2a1912ba73beedc2
leo.chen <leo.chen@unicornfintech.com>
2018-05-02 18:30:27 +0800

    add chrome-extension smart-zoom

---
+++ chrome-extension/SmartZoom/smartZoomApp.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  var currentZoomElement;
+  var body = $(document.body);
+  $('*').bind('mousedown', function (e) {
+    var e = e || window.event;
+    if (e.button == 1) {
+      e.stopPropagation();
+      e.preventDefault();
+      console.log(currentZoomElement);
+      if (currentZoomElement != null) {
+        currentZoomElement.zoomTo({ targetsize: 1, scalemode: 'width', duration: 300, closeclick: true });
+        currentZoomElement = null;
+      } else {
+        var element = $(this);
+        console.log(element);
+        if(element!=null && element.text().length>0 &&element.context != body.context){
+          element.zoomTo({ targetsize: 0.75, scalemode: 'width', duration: 300, closeclick: true, root: $(document.body) });
+          currentZoomElement = body;
+        }
+      }
+      $('.noScroll').removeClass('noScroll');
+    }
+  });
+});
