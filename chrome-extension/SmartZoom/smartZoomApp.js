5b472617549c3e5ef7eadb19751944812def97b1
leo.chen <leo.chen@unicornfintech.com>
2018-05-15 17:08:03 +0800

    fix something chrome-extension

--- chrome-extension/SmartZoom/smartZoomApp.js
+++ chrome-extension/SmartZoom/smartZoomApp.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
   var currentZoomElement;
   var body = $(document.body);
-  $('*').bind('mousedown', function (e) {
+  $('p,div,span,label,table').bind('mousedown', function (e) {
     var e = e || window.event;
     if (e.button == 1) {
       e.stopPropagation();
       e.preventDefault();
       console.log(currentZoomElement);
       if (currentZoomElement != null) {
-        currentZoomElement.zoomTo({ targetsize: 1, scalemode: 'width', duration: 300, closeclick: true });
+        currentZoomElement.zoomTo({ targetsize: 1, scalemode: 'width', duration: 100, closeclick: true });
         currentZoomElement = null;
       } else {
         var element = $(this);
         console.log(element);
         if(element!=null && element.text().length>0 &&element.context != body.context){
-          element.zoomTo({ targetsize: 0.75, scalemode: 'width', duration: 300, closeclick: true, root: $(document.body) });
+          element.zoomTo({ targetsize: 1, scalemode: 'width', duration: 100, closeclick: true, root: $(document.body) });
           currentZoomElement = body;
         }
       }
