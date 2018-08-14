67663635f25b34d651d91aa03a1949be59135967
leo.chen <leo.chen@unicornfintech.com>
2018-08-14 15:23:23 +0800

    update regexSearch2 implement

--- chrome-extension/RegexSearch2/js/background.js
+++ chrome-extension/RegexSearch2/js/background.js
@@ -1,24 +1,69 @@
 $(document).ready(function () {
 
-    window.serachAndMark = function (regexPattern) {
-        if (regexPattern != null && regexPattern.length > 0 && checkPattern(regexPattern)) {
-            var regex = new RegExp('('+regexPattern+')', 'g');
-            $('*').each(function () {
-                var element = $(this);
-                var text = element.clone().children().remove().end().text();
-                $('mark').unwrap();
-                if (regex.test(text)) {
-                   element.html(element.html().replace(regex,'<mark>$1</mark>'));
-                }
-            })
-        } else {
-            $('*').each(function () {
-                var element = $(this);
-                element.find('mark').unwrap();
-            })
-        }
+    var markElements = null;
+    var markIndex = 0;
+    var currentElement = null;
+    var currentRegex = null;
 
+    window.serachAndMark = function (regexPattern, regexFlag) {
+        var body = $('body');
+        markIndex = 0;
+        markElements = null;
+        currentElement = null;
+        currentRegex = regexPattern;
+        body.unmark();
+        if (regexPattern != null && regexPattern.length > 0 && checkPattern(regexPattern)) {
+            var regex = new RegExp('(' + regexPattern + ')', 'gi');
+            body.markRegExp(regex);
+            markElements = $('mark');
+            selectElement(markElements[0]);
+        }
     };
+
+    window.nextMark = function () {
+        if (markElements != null && markElements.length > 0) {
+            markIndex++;
+            if (markIndex >= markElements.length) {
+                markIndex = markElements.length - 1;
+            }
+            selectElement(markElements[markIndex], window.nextMark);
+        }
+    };
+
+    window.previousMark = function () {
+        if (markElements != null && markElements.length > 0) {
+            markIndex--;
+            if (markIndex <= 0) {
+                markIndex = 0;
+            }
+            selectElement(markElements[markIndex], window.previousMark);
+        }
+    };
+
+    window.getCurrentRegex = function () {
+        return currentRegex;
+    };
+
+    function selectElement(element, passToNext) {
+        if (element != null && element != currentElement) {
+            if ($(element).is(":hidden")) {
+                if (passToNext != null) {
+                    passToNext();
+                }
+                return false;
+            }
+            if (currentElement != null) {
+                $(currentElement).removeClass('mark-current');
+            }
+            currentElement = element;
+            if (currentElement != null) {
+                $('html, body').animate({
+                    scrollTop: $(currentElement).offset().top - 150
+                }, 100, 'linear');
+                $(currentElement).addClass('mark-current');
+            }
+        }
+    }
 
     function checkPattern(pattern) {
         var result = true;
--- chrome-extension/RegexSearch2/js/popup.js
+++ chrome-extension/RegexSearch2/js/popup.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
     var regexBox = $("#regexBox");
+    var nextBtn = $('#nextBtn');
+    var previousBtn = $('#previousBtn');
     var searching = false;
-    regexBox.bind('keyup', function () {
+
+    regexBox.bind('keypress', function (e) {
+        if (e.ctrlKey || e.which == 13) {
+            return false;
+        }
+    });
+
+    regexBox.bind('keydown', function (e) {
+        if (e.ctrlKey && e.keyCode == 13) {
+            previousBtn.click();
+        } else if (e.which == 13) {
+            nextBtn.click();
+        }
+    });
+
+    regexBox.bind('keyup', function (e) {
+        if (e.which == 17 || e.which == 13) {
+            return false;
+        }
         var regexPattern = $(this).val();
         if (!searching) {
             searching = true;
@@ -13,7 +33,19 @@ $(document).ready(function () {
         }
     });
 
-    function cleanPattern(pattern){
+    nextBtn.bind('click', function () {
+        chrome.tabs.executeScript({
+            code: "nextMark();"
+        });
+    });
+
+    previousBtn.bind('click', function () {
+        chrome.tabs.executeScript({
+            code: "previousMark();"
+        });
+    });
+
+    function cleanPattern(pattern) {
         pattern = pattern.split("\\").join("\\\\");
         pattern = pattern.split("\'").join("\\'");
         pattern = pattern.split("\"").join("\\\"");
@@ -21,5 +53,12 @@ $(document).ready(function () {
         return pattern;
     }
 
+    chrome.tabs.executeScript({
+        code: "getCurrentRegex();"
+    }, function (result) {
+        regexBox.val(result);
+    });
+
     regexBox.focus();
+
 });
