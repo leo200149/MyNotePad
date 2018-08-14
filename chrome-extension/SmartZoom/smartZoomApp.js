b74c85bfe6f70dd7bae47580234b64fefc7c75c6
leo.chen <leo.chen@unicornfintech.com>
2018-08-14 12:29:26 +0800

    update chrome extension smartzoom.

--- chrome-extension/SmartZoom/smartZoomApp.js
+++ chrome-extension/SmartZoom/smartZoomApp.js
@@ -1,24 +1,8 @@
-$(document).ready(function () {
-  var currentZoomElement;
-  var body = $(document.body);
-  $('p,div,span,label,table').bind('mousedown', function (e) {
-    var e = e || window.event;
-    if (e.button == 1) {
-      e.stopPropagation();
-      e.preventDefault();
-      console.log(currentZoomElement);
-      if (currentZoomElement != null) {
-        currentZoomElement.zoomTo({ targetsize: 1, scalemode: 'width', duration: 100, closeclick: true });
-        currentZoomElement = null;
-      } else {
-        var element = $(this);
-        console.log(element);
-        if(element!=null && element.text().length>0 &&element.context != body.context){
-          element.zoomTo({ targetsize: 1, scalemode: 'width', duration: 100, closeclick: true, root: $(document.body) });
-          currentZoomElement = body;
-        }
-      }
-      $('.noScroll').removeClass('noScroll');
-    }
-  });
+$('p,div,span,label,table').bind('mousedown', function (e) {
+  var e = e || window.event;
+  if (e.button == 1) {
+    e.stopPropagation();
+    e.preventDefault();
+    zoom.to({element:e.target,duration:400});
+  }
 });
