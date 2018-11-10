ee35aa2c26562f7361ffaf544a5e6d4f0c81200a
leo200149 <leo200149@gmail.com>
2018-11-10 20:55:18 +0800

    update autoplay ami game

--- chrome-extension/AutoPlay/js/app.js
+++ chrome-extension/AutoPlay/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var autoPlayAniGamer = Storage.getValue('autoPlayAniGamer');
-    function init(){
+	var isHideTop = false;
+    function autoPlay(){
         $('#adult').click();
         setTimeout(playWithAutoNextAndFullScreen,3000);
     }
@@ -20,42 +21,70 @@ $(document).ready(function () {
         });
         autoPlaySwitch.prop("checked",autoPlayAniGamer);
     }
+	function hideTop(){
+		function hideOrShow(){
+			if(isHideTop){
+				$('.sky').hide();
+				$('.mainmenu').hide();
+				$('.container-player').css('padding','0');
+				$('.container-player').css('max-width','100%');
+				$('.BH_background').css('margin-top','0');
+				$('.player').css('margin','0');
+				$('.anime-title').hide();
+				$('.data').hide();
+				$('.news_list').hide();
+				$('.whilebg').hide();
+				$('.footer').hide();
+				$('.bullet-send').hide();
+			}else{
+				$('.sky').show();
+				$('.mainmenu').show();
+				$('.container-player').css('max-width','');
+				$('.BH_background').css('margin-top','');
+				$('.player').css('margin-top','');
+				$('.anime-title').show();
+				$('.data').show();
+				$('.news_list').show();
+				$('.whilebg').show();
+				$('.footer').show();
+				$('.bullet-send').show();
+			}
+			isHideTop = !isHideTop;
+		}
+		var identEnable = $('.vjs-indent-enable')
+		var identEnableBtn = $('.vjs-indent-button');
+		if(identEnable.html()!=null){
+			isHideTop = true;
+		}
+		identEnableBtn.bind('click',function(){
+			hideOrShow();
+		})
+		hideOrShow();
+	}
+	
     function playWithAutoNextAndFullScreen(){
         var video = $('video')[0];
-        var isFullScreen = false;
         if (video != null){
             $(video).bind('ended',function(){
                 if (autoPlayAniGamer){
-                    isFullScreen = false;
                     $('.playing').next().find('a')[0].click();
-                }
-            });
-            $(video).bind('play',function(){
-                if (autoPlayAniGamer){
-                    if (!isFullScreen){
-                        console.log('fullscreen')
-                        setTimeout(fullscreen,5000)
-                        isFullScreen = true;
-                    }
                 }
             });
         }else {
             setTimeout(playWithAutoNextAndFullScreen,1000);
         }
     }
-    function fullscreen(){
-        $('.vjs-fullscreen-control').click();
-    }
+	
     function checkReady(){
         if ($('.R18')!=null) {
-            init();
+			hideTop();
+            autoPlay();
             return
         }
         setTimeout(checkReady,1000);
     }
 
     initBtn();
-
     if(autoPlayAniGamer){
         setTimeout(checkReady,1000)
     }
--- chrome-extension/AutoPlay/js/background.js
+++ chrome-extension/AutoPlay/js/background.js
@@ -1,9 +1,2 @@
 $(document).ready(function () {
-    function fullscreen(){
-        chrome.tabs.executeScript(null,{code:"console.log('test background');$('.vjs-fullscreen-control').click()"});
-        console.log('test background');
-    }
-    chrome.tabs.onUpdated.addListener(function (tabId) {
-        setTimeout(fullscreen,6000);
-    })
 });
